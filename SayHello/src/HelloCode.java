edac7ba91847bcc33babdbc4994672bb30fbb4ff
nilmast <1488nile>
2023-03-06 15:23:35 +0300

    Initial commit

---
+++ SayHello/src/HelloCode.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+import static java.lang.System.*;
+
+public class HelloCode {
+    public static void main(String[] args) throws IOException {
+        out.write("Hello World".getBytes());
+    }
+}
